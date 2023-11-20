ca46a97b297a27827c818cf0efacc68b755336ee
Matt Limbu <43614027+lmbu@users.noreply.github.com>
2023-11-20 11:02:59 -0500

    Initial Commit

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Matt Limbu on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
